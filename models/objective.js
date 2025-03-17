8bf32941af8f2123605618e35fa344743b889805
AlexisCubilla <alexiscubilla500@gmail.com>
2025-03-17 16:34:06 +0000

    cambios en objective

--- models/objective.js
+++ models/objective.js
@@ -8,20 +8,34 @@ export default (sequelize, DataTypes) => {
     description: DataTypes.STRING,
     poaId: {
       type: DataTypes.UUID,
-      allowNull: false,
       references: {
         model: 'poas',
         key: 'id'
       }
     },
-    parent: DataTypes.UUID,
+    objective_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'objectives', 
+        key: 'id'
+      }
+    },
     type: DataTypes.STRING,
     code: DataTypes.STRING,
     folderId: DataTypes.UUID
   }, {
     tableName: 'objectives',
     schema: process.env.SCHEMA,
-    timestamps: false
+    timestamps: false,
+    validate: {
+      onlyOneForeignKey() {
+        const keys = [this.poaId, this.objetive_id];
+        const filled = keys.filter(k => k !== null && k !== undefined);
+        if (filled.length !== 1) {
+          throw new Error('Debe haber exactamente UNO (y sólo uno) de: poaId, objetive_id');
+        }
+      }
+    }
   });
 
   return Objective;
